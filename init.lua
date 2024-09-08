d0c778ea98ce4671fb658f495810ead02168a017
Daryll Ko <daryll.ko@gmail.com>
2024-09-08 13:59:36 +0800

    chore: install `mdx-analyzer`

--- init.lua
+++ init.lua
@@ -610,6 +610,7 @@ require('lazy').setup({
         svelte = {},
         pyright = {},
         rust_analyzer = {},
+        mdx_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
