38269489e0042b35c11dfd23508747ac3e438ac4
Daryll Ko <daryll.ko@gmail.com>
2024-11-03 12:45:24 +0800

    update: change `typst-preview` version

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,7 +5,7 @@
 return {
   'chomosuke/typst-preview.nvim',
   lazy = false, -- or ft = 'typst'
-  version = '0.3.*',
+  version = '1.*',
   build = function()
     require('typst-preview').update()
   end,
