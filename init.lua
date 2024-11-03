b31425e428248fa58618b57d132ebac3269a79b6
Daryll Ko <daryll.ko@gmail.com>
2024-11-03 12:52:10 +0800

    update: add `typst-lsp`

--- init.lua
+++ init.lua
@@ -611,6 +611,7 @@ require('lazy').setup({
         pyright = {},
         rust_analyzer = {},
         mdx_analyzer = {},
+        typst_lsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
