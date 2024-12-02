a4aa101fd54eef5626fdf2f0d1ad1f76843c49eb
Daryll Ko <daryll.ko@gmail.com>
2024-12-02 15:55:29 +0800

    add: include `hls`

--- init.lua
+++ init.lua
@@ -628,6 +628,7 @@ require('lazy').setup({
           end,
           settings = {},
         },
+        hls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
